ec389a05ade9dc8989f308e470f17b3cf19edd4f
Vinay Sharma <vinaysharma7811@gmail.com>
2021-09-25 21:57:29 +0530

    feature: submit button designed

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import { View, TextInput, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Pressable, TextInput } from 'react-native';
 
 export const Home = () => (
   <View style={styles.container}>
     <TextInput style={styles.input} />
+
+    <Pressable
+      style={({ pressed }) => [
+        styles.button,
+        ...(pressed ? [styles.buttonPressed] : []),
+      ]}
+    >
+      <Text style={styles.buttonText}>Submit</Text>
+    </Pressable>
   </View>
 );
 
@@ -17,5 +26,18 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     paddingHorizontal: 20,
     backgroundColor: 'white',
+  },
+  button: {
+    padding: 15,
+    marginTop: 20,
+    borderRadius: 5,
+    backgroundColor: 'black',
+  },
+  buttonPressed: {
+    opacity: 0.5,
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
   },
 });
