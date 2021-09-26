36e55d821b0e5d252041be98ae1280cf494e8fea
Vinay Sharma <vinaysharma7811@gmail.com>
2021-09-26 16:45:13 +0530

    feature: submit and reset button logic added

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,20 +1,52 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import { View, Text, StyleSheet, Pressable, TextInput } from 'react-native';
 
-export const Home = () => (
-  <View style={styles.container}>
-    <TextInput style={styles.input} />
+export const Home = () => {
+  const [task, setTask] = useState('');
 
-    <Pressable
-      style={({ pressed }) => [
-        styles.button,
-        ...(pressed ? [styles.buttonPressed] : []),
-      ]}
-    >
-      <Text style={styles.buttonText}>Submit</Text>
-    </Pressable>
-  </View>
-);
+  const resetTask = useCallback(() => {
+    setTask('');
+  }, []);
+
+  const textChangeHandler = useCallback(value => {
+    setTask(value);
+  }, []);
+
+  const submitPressHandler = useCallback(() => {
+    resetTask();
+    console.log('task captured', task);
+  }, [task, resetTask]);
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        value={task}
+        style={styles.input}
+        onChangeText={textChangeHandler}
+      />
+
+      <Pressable
+        onPress={submitPressHandler}
+        style={({ pressed }) => [
+          styles.button,
+          ...(pressed ? [styles.buttonPressed] : []),
+        ]}
+      >
+        <Text style={styles.buttonText}>Submit</Text>
+      </Pressable>
+
+      <Pressable
+        onPress={resetTask}
+        style={({ pressed }) => [
+          styles.button,
+          ...(pressed ? [styles.buttonPressed] : []),
+        ]}
+      >
+        <Text style={styles.buttonText}>Reset</Text>
+      </Pressable>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
